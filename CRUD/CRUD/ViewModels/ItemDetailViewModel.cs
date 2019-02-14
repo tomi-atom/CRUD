5caf7e7822ad637aa358cfd066609687092bdc6b
tomi <tomi.firman@student.unri.ac.id>
2019-02-14 08:25:42 +0700

    Add project files.

---
+++ CRUD/CRUD/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CRUD.Models;
+
+namespace CRUD.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
